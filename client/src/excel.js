888908eeb19ac112ba74a72168c9bdd873a1acf0
steven tsan <fabooshus@gmail.com>
2024-02-07 06:57:43 -0800

    need to get file pushed

--- client/src/excel.js
+++ client/src/excel.js
@@ -5,12 +5,8 @@ function ExcelToJsonConverter() {
     const [file, setFile] = useState(null);
     const [jsonData, setJsonData] = useState(""); // this just displays to the user the JSON object
     var jsonFile = null;
-    var IPGeolocationAPI = require('ip-geolocation-api-javascript-sdk/IPGeolocationAPI.js');
-    var ipgeolocationApi = new IPGeolocationAPI("dc0f74a1574b4e0f89c55fe33dc2124a", true); // PUT YOUR API KEY HERE
-    var GeolocationParams = require('ip-geolocation-api-javascript-sdk/GeolocationParams.js');
-    var geolocationParams = new GeolocationParams();
-    var maxLength = 50;
-    geolocationParams.setLang('en');
+    var maxLength = 45;
+    var response = null; // this is the response from the API
     const handleConvert = () => {
         if (file) {
             const reader = new FileReader();
@@ -29,78 +25,71 @@ function ExcelToJsonConverter() {
         }
     };
     // Bulk Geolocations Lookup is 5 steps
-    const handleBulkGeolocationsLookup = (jf) => {
-        jsonFile = jf;
+    async function handleBulkGeolocationsLookup(jf) {
+        // make jf only have unique entries inside the json.
+        var unique = {};
+        var distinct = [];
+        for (var i in jf) {
+            if (typeof unique[jf[i]['IP']] == "undefined") {
+                distinct.push(jf[i]);
+            }
+            unique[jf[i]['IP']] = 0;
+        }
+        jf = distinct;
+        console.log(jf);
         // 1. Get the IP addresses from the JSON data
-        var ipAddresses = Object.entries(jf).map(([key, value]) => {
-            const ipAddress = value;
-            const parsedIpAddress = ipAddress['IP'];
-            if (ipAddress['IP'] !== undefined) {
-                return parsedIpAddress;
-            }
-            return null;
-        });
-        // remove duplicate IP addresses
-        const uniqueIpAddresses = [...new Set(ipAddresses)];
-        ipAddresses = uniqueIpAddresses;
-        console.log(ipAddresses);
-        //geolocationParams.setFields('country_code3,state_prov');// Specify the required fields/objects for multiple IP addresses
-        // if length is larger than maxLength, then split the array into multiple arrays of maxLength
-        if (Object.entries(ipAddresses).length > maxLength) {
-            console.log("The number of queries in the request is: " + Object.entries(ipAddresses).length);
-            const splitArrays = [];
-            while (ipAddresses.length > 0) {
-                splitArrays.push(ipAddresses.splice(0, maxLength));
-                console.log(splitArrays);
-                // Query geolocation for multiple IP addresses and all fields
-                geolocationParams.setIPAddresses(splitArrays);
-                // 2. Get the state and country from the IP address
-                ipgeolocationApi.getGeolocation(handleResponse, geolocationParams); // Result goes to handleResponse
-            }
-        } else {
-            // Query geolocation for multiple IP addresses and all fields
-            geolocationParams.setIPAddresses(ipAddresses);
-            // 2. Get the state and country from the IP address
-            ipgeolocationApi.getGeolocation(handleResponse, geolocationParams); // Result goes to handleResponse
-        }
-        setJsonData(JSON.stringify(jsonFile, null, 2)); // this just displays to the user the JSON object
-        // 4. Put the JSON object into an Excel file
-        const worksheet = XLSX.utils.json_to_sheet(jsonFile);
-        const workbook = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-        const excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+        for (const [key, value] of Object.entries(jf)) {
 
-        // 5. Download the Excel file
-        const blob = new Blob([excelBuffer], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = "output.xlsx";
-        link.click();
-
-    };
-
-    // Function to handle response from IP Geolocation API
-    async function handleResponse(response) {
-        console.log(response);
-        if (response["message"]) {
-            console.log(response["message"]);
-            return;
-        }
-        // 3. For jsonData object (jsonFile), push response into the JSON object row as well
-        for (var i = 0; i < jsonFile.length; i++) {
-            const ipAddress = jsonFile[i]["ip"];
-            const matchingResponse = response.find((res) => res["ip"] === ipAddress);
-            if (matchingResponse) {
-                jsonFile[i]["country_code3"] = matchingResponse["country_code3"];
-                // If country_code3 == USA, then add the state_prov to the JSON object row as well
-                if (matchingResponse["country_code3"] === "USA") {
-                    jsonFile[i]["state_prov"] = matchingResponse["state_prov"];
+            await new Promise((resolve) => setTimeout(resolve, 625));
+            
+            const row = value;
+            if (row['IP'] !== undefined) {
+                const endpoint = `http://ip-api.com/json/${row['IP'].trim()}?fields=status,message,countryCode,regionName`;
+                const xhr = new XMLHttpRequest();
+                xhr.open("GET", endpoint, true);
+                xhr.onreadystatechange = function () {
+                    response = xhr;
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+                        const data = JSON.parse(xhr.responseText);
+                        console.log(data);
+                        if (data["status"] === "fail") {
+                            console.log(data["message"]);
+                        } else {
+                            jf[key]['countryCode'] = data['countryCode'];
+                            if (data['countryCode'] === "US") {
+                                jf[key]['regionName'] = data['regionName'];
+                            }
+                        }
+                        console.log(jf[key]);
+                    }
+                };
+                xhr.send();
+                // if 429 too many requests, wait 60 seconds
+                if (xhr.status === 429) {
+                    console.log("Waiting 60 seconds to avoid rate limiting");
+                    await new Promise((resolve) => setTimeout(resolve, 60000));
                 }
+                console.log(response);
             }
         }
-    }
-
+        console.log(jf);
+        jsonFile = jf;
+        setJsonData(JSON.stringify(jf, null, 2));
+         // 4. Put the JSON object into an Excel file
+         const worksheet = XLSX.utils.json_to_sheet(jsonFile);
+         const workbook = XLSX.utils.book_new();
+         XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+         const excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+ 
+         // 5. Download the Excel file
+         const blob = new Blob([excelBuffer], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" });
+         const url = URL.createObjectURL(blob);
+         const link = document.createElement("a");
+         link.href = url;
+         link.download = "output.xlsx";
+         link.click();
+    };
+    
     return (
         <div>
             <input
